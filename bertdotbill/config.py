70c98cd0089f1a502e45954cef6df592ff671ce7
Bert Tejeda <etejeda@tecknicos.com>
2022-06-26 12:17:10 -0400

    Fixed handling of config from cli args

--- bertdotbill/config.py
+++ bertdotbill/config.py
@@ -13,16 +13,19 @@ class AppConfig():
 
   def initialize(self, **kwargs):
 
-    args = kwargs['args']
+    logger.info("Initializing config")
+    args = kwargs.get('args', {})
     verify_tls = kwargs.get('verify_tls', False)
     # Initialize App Config
     initial_data = {
     'environment': os.environ
     }  
 
+    config_file_uri = args.get('config_file') or default_config_file_name
+    logger.info(f"Config file URI is {config_file_uri}")
     # Initialize App Config
     config = Config(
-        config_file_uri=default_config_file_name,
+        config_file_uri=config_file_uri,
         default_value=default_settings,
         initial_data=initial_data,
         args=args,
