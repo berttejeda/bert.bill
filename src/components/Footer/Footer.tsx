3b891888f0d945db23f2b4acfb294ed1090fd9de
Bert Tejeda <etejeda@tecknicos.com>
2023-04-05 14:42:43 -0400

    Syntax

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -75,7 +75,7 @@ export default function Footer(lesson, {children}) {
           className='w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
           type="text"
           id="wsUrl"
-          placeholder="e.g. ws://127.0.0.1:10001/terminals/<terminalID>"
+          placeholder="e.g. ws://127.0.0.1:10001/terminals/$terminalID"
           aria-label="Websocket URL"
           aria-describedby="wsUrl"
           onChange={nameInputChangeHandler}
