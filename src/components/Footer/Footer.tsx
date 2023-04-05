1b2deea4e610692758e4cb9aa061a1417aac6d55
Bert Tejeda <etejeda@tecknicos.com>
2023-04-05 06:47:52 -0400

    Stylize Websocket form

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -29,32 +29,74 @@ export default function Footer(lesson, {children}) {
         console.log(e)
       }
 
-  }, []);   
+  }, []);
+
+
+  const [EnteredWsUrl, setEnteredWsUrl] = useState('')
+  const [EnteredWsUrlIsValid, setEnteredWsUrlIsValid] = useState(false)
+  const [EnteredWsUrlTouched, setEnteredWsUrlTouched] = useState(false)
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredWsUrl(event.target.value)
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault()
+
+    setEnteredWsUrlTouched(true)
+
+    if (EnteredWsUrl.trim() === '' || !EnteredWsUrl.startsWith("ws")) {
+      setEnteredWsUrlIsValid(false)
+      return
+    }
+
+    setEnteredWsUrlIsValid(true)
+
+    handleSocketChange(EnteredWsUrl)
+
+    setEnteredWsUrl('')
+
+  }
+
+  const nameInputIsInvalid = !EnteredWsUrlIsValid && EnteredWsUrlTouched
+
+  const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid mb-3'
+    : 'form-control mb-3'  
 
   return (
 
   <div className={footerClasses}>
       <div>
-        <header>
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder={'You can enter a different webterminal socket URL'}
-              aria-label="Websocket URL"
-              aria-describedby="wsUrl"
-              value={input.value}
-              onChange={(value) => setInput(value.target.value)}
-              defaultValue={wsUrl}
-            />
-            <InputGroup.Text id="wsUrl"></InputGroup.Text>
-            <button onClick={() => handleSocketChange(input)}>Connect</button>
-          </InputGroup>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label className='block text-sm gp rt' htmlFor='name'>Specify alternate Websocket Address</label>
+        <input
+          className='w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          type="text"
+          id="wsUrl"
+          placeholder="e.g. ws://127.0.0.1:10001/terminals/<terminalID>"
+          aria-label="Websocket URL"
+          aria-describedby="wsUrl"
+          onChange={nameInputChangeHandler}
+          value={EnteredWsUrl}
+        />
+
+        <button className='btn ho xi ye ml-3'>Connect</button>
+        {nameInputIsInvalid && (
+          <p className='block error-text text-sm gp rt'>
+          Websocket URL is empty or invalid!
+          </p>
+        )}
+      </div>
+    </form>
+
           { (wsUrl) ? <div></div> : <div>Couldn't connect to Webterminal agent<br />
             You can start one locally via docker with:<br />
             <code>docker run -it --rm --network=host --name webterminal berttejeda/bill-webterminal</code><br />
             Make sure to refresh this page once your agent is running.
             Read more at <a href="https://github.com/berttejeda/bert.bill.webterminal">https://github.com/berttejeda/bert.bill.webterminal</a></div>
           }
-        </header>
         { (wsUrl && lesson) ?
           <XTerm key={wsUrl} wsUrl={wsUrl} lesson={lesson} />
           :
