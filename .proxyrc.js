529a04c8b7c2ee3993bc8433478bbb0e36151367
Bert Tejeda <etejeda@tecknicos.com>
2023-04-26 22:46:43 -0400

    Fixed proxy specification
    Using localhost was producing errors, so I switched to 127.0.0.1

--- .proxyrc.js
+++ .proxyrc.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
       createProxyMiddleware("/api", {
-        target: "http://localhost:10000/",
+        target: "http://127.0.0.1:10000/",
       })
   );
 };
